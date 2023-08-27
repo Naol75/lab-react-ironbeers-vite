9b739590e9191912b4f058947d6026e28b4317a8
OmarNavarro <omar.n.o.92@gmail.com>
2023-08-27 14:08:27 +0200

    css-added

--- src/pages/BeerDetailsPage.jsx
+++ src/pages/BeerDetailsPage.jsx
@@ -17,26 +17,37 @@ function BeerDetailsPage() {
     try {
       const response = await axios.get(
         `https://ih-beers-api2.herokuapp.com/beers/${beerId}`
-        );
-        setBeer(response.data);
+      );
+      setBeer(response.data);
     } catch (error) {
-        console.log(error)
+      console.log(error);
     }
   };
 
   if (!beer) {
-    return <div className="moon-loader beer-background " ><MoonLoader color="rgb(117, 100, 75)" /></div>
+    return (
+      <div className="moon-loader beer-background ">
+        <MoonLoader color="rgb(117, 100, 75)" />
+      </div>
+    );
   }
 
   return (
-    <div>
-      <h1>{beer.name}</h1>
-      <img src={beer.image_url} alt={beer.name} />
-      <p>{beer.tagline}</p>
-      <p>First Brewed: {beer.first_brewed}</p>
-      <p>Attenuation Level: {beer.attenuation_level}</p>
-      <p>{beer.description}</p>
-      <p>Created by: {beer.contributed_by}</p>
+    <div className="random-beer-page">
+      <h1 className="title">{beer.name}</h1>
+      <div className="wrapper random-container" >
+
+      <div>
+        <img className="random-img" width={50} src={beer.image_url} alt={beer.name} />
+      </div>
+      <div className="random-beer-text-container" >
+        <p>{beer.tagline}</p>
+        <p>First Brewed: {beer.first_brewed}</p>
+        <p>Attenuation Level: {beer.attenuation_level}</p>
+        <p>{beer.description}</p>
+        <p>Created by: {beer.contributed_by}</p>
+      </div>
+      </div>
     </div>
   );
 }
