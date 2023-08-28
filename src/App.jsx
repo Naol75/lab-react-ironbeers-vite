c7e2734be986bbe00c48560635e8b7644620b1db
OmarNavarro <omar.n.o.92@gmail.com>
2023-08-28 08:59:25 +0200

    styles-added

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,12 @@ import AllBeersPage from "./pages/AllBeersPage";
 import BeerDetailsPage from "./pages/BeerDetailsPage";
 import RandomBeerPage from "./pages/RandomBeerPage";
 import AddBeerPage from "./pages/AddBeerPage";
-import { ThemeContext } from "./theme.context";
-import { useContext } from "react";
+
 
 function App() {
 
   return (
-    <div >
+    < >
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
@@ -22,7 +21,7 @@ function App() {
         <Route path="/new-beer" element={<AddBeerPage />} />
         <Route path="/beers/:beerId" element={<BeerDetailsPage />} />
       </Routes>
-    </div>
+    </>
   );
 }
 
